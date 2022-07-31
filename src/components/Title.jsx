9f799750ddad073f9bed328310e2d341aaeebe7e
KONIMAO <66402278+KONIMAO@users.noreply.github.com>
2022-07-31 17:31:47 +0900

    アップロード

---
+++ src/components/Title.jsx
@@ -0,0 +1,11 @@
+const Title = () => {
+    return(
+        <header>
+            <h1>Image Search App</h1>
+            <p>By <a href="https://unsplash.com/">Unsplash</a></p>
+            <p>英単語で好きな画像を検索してください。</p>
+        </header>
+    );
+}
+ 
+export default Title;
